9c4c3027fa1a0c72225162c852153d5b3de33065
danielgarciarivas <daniel@t1comercios.com>
2025-08-22 11:35:30 -0600

    api prod

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+export const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://t1mkp-api.vercel.app';
 
 export const ROUTES = {
   HOME: '/',
